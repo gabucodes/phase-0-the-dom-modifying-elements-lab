fc821d12412b8f2a467eacf04213d29fa68e206e
Gabriel <kipkoechgabriel27@gmail.com>
2025-03-02 23:07:02 +0300

    completes assigment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+// Remove the <main> with id 'main'
+document.querySelector('main#main').remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set its id to 'victory'
+newHeader.id = 'victory';
+
+// Set the text inside the <h1> element
+newHeader.innerHTML = "YOUR-NAME is the champion"; // Replace YOUR-NAME with your actual name
+
+// Append the new <h1> to the document body
+document.body.appendChild(newHeader);
